87d45f3d83fd4d1bb1d71033e68b7c34baa74357
zzy <zyzhou@umich.edu>
2020-12-21 10:22:21 +0800

    clion_proj added

---
+++ clion_proj/cmake-build-debug/CMakeFiles/clion_proj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_proj.dir/5_longestPalindrome.cpp.o"
+  "CMakeFiles/clion_proj.dir/main.cpp.o"
+  "clion_proj"
+  "clion_proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
